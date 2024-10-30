7ccfd8a62c6519e6c57f11e035c9e497a9499fe2
Edgar Mendez <edgarmendezcruz75@gmail.com>
2024-10-30 01:55:50 -0500

    correccion de origen

--- app/server.js
+++ app/server.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://admina.netlify.app",
     credentials: true,
   })
 );
