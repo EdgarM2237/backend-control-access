28e3b13434df815712bd12164e7788acea7d181d
Edgar Mendez <edgarmendezcruz75@gmail.com>
2024-10-30 21:50:49 -0500

    Creacion query para userlogs

--- app/models/User_logs.js
+++ app/models/User_logs.js
@@ -1,7 +1,7 @@
 import connection from '../config/db.js';
 
 export const user_logs = (req, res) => {
-  const query = 'SELECT users.user_name,users.user_email,devices.device_name,userlogs.card_uid,userlogs.estado,userlogs.user_date FROM userlogs JOIN users ON userlogs.user_id = users.user_id JOIN devices ON userlogs.device_id = devices.device_id ORDER BY userlogs.user_date DESC';
+  const query = 'SELECT users.user_id,users.user_name,users.user_img,users.user_serial,users.user_email,devices.device_name,userlogs.card_uid,userlogs.estado,userlogs.user_date FROM userlogs JOIN users ON userlogs.user_id = users.user_id JOIN devices ON userlogs.device_id = devices.device_id ORDER BY userlogs.user_date DESC';
   connection.query(query, (err, results) => {
     if (err) {
       console.error('Error fetching user logs:', err);
